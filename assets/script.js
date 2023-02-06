6250bdb4399408f60dede4db7bc7360037d815f7
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-02-06 17:15:07 +0000

    javascript

--- assets/script.js
+++ assets/script.js
@@ -23,13 +23,20 @@ fetch(URL, {
             urlText.append('star-sign')
 });
 // Tamara's work right-side
-fetch("https://aztro.sameerkumar.website?sign=aquarius&day=today",
-    { method: "POST" }
-)
-    .then(response => response.json())
-    .then(response => console.log(response))
-    .catch(err => console.error(err));
-    
-    let description = json.description
-    console.log(description)
+function horoscope() {
+    fetch("https://aztro.sameerkumar.website?sign=aquarius&day=today",
+        { method: "POST" }
+    )
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            let description = response.description;
+            console.log(description)
+            //added DOM element to render description on the page
+            document.querySelector(".card-text").textContent = description;
+        })
+        .catch(err => console.error(err));
 
+}
+
+horoscope();
