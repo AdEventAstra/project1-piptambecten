a451b3b0e233ecf8e4287de67c2a04e5e4f412de
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-02-07 16:31:56 +0000

    nasa api work in progress

--- assets/script.js
+++ assets/script.js
@@ -12,16 +12,16 @@ const URL = 'https://aztro.sameerkumar.website/?sign=aries&day=today';
 fetch(URL, {
     method: 'POST'
 })
-.then(response => response.json())
-.then(json => {
-    const date = json.description;
-    console.log(date);
-        var profileUrl = response.description;
+    .then(response => response.json())
+    .then(json => {
+        const date = json.description;
+        console.log(date);
+        var profileUrl = json.description;
         console.log(profileUrl)
         var urlText = document.querySelectorId('star-sign')
-            urlText.setAttribute('src', profileUrl)
-            urlText.append('star-sign')
-});
+        urlText.setAttribute('src', profileUrl)
+        urlText.append('star-sign')
+    });
 // Tamara's work right-side
 function horoscope() {
     fetch("https://aztro.sameerkumar.website?sign=aquarius&day=today",
@@ -40,3 +40,27 @@ function horoscope() {
 }
 
 horoscope();
+
+// Nasa API
+// https://images-api.nasa.gov/search?q=planets
+function getNasa() {
+    fetch("https://images-api.nasa.gov/search?q=planets")
+        .then(response => response.json())
+        .then(response => {
+            console.log(response);
+            console.log(response.collection.items[0].links[0]);
+            let imageNasa = response.collection.items[0].links[0].href;
+            //added DOM element to render description on the page
+            // document.querySelector(".API").style.background = imageNasa;
+            document.querySelector("body").style.backgroundImage = `url(${imageNasa})`;
+            document.querySelector()
+
+        })
+        .catch(err => console.error(err));
+
+}
+getNasa();
+
+
+
+
