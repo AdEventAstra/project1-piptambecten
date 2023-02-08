1cc4ee3f27dd720f4b77c1176ce47e3c54bfce72
TamaraJoniec <117231191+TamaraJoniec@users.noreply.github.com>
2023-02-08 14:51:30 +0000

    pasted missing bits back in

--- assets/script.js
+++ assets/script.js
@@ -1,13 +1,44 @@
-// Rebecca's work navbar
+// Rebecca's work navbar buttons
+//let startButton = document.querySelector("#start");
+
+let aries = document.querySelector("#aries");
+let taurus = document.querySelector("#taurus");
+let gemini = document.querySelector("#gemini");
+let cancer = document.querySelector("#cancer");
+let leo = document.querySelector("#leo");
+let virgo = document.querySelector("#virgo");
+let scorpio = document.querySelector("#scorpio");
+let sagittarius = document.querySelector("#sagittarius");
+let capricorn = document.querySelector("#capricorn");
+let aquarius = document.querySelector("#aquarius");
+let pisces = document.querySelector("#pisces");
 
 // Tennisson's work NASA
 
 // Patricia's work left-side
-// fetch("https://aztro.sameerkumar.website/?sign=aquarius&date_range=aquarius")
-// .then(response => response.json())
-// .then(function(response){
-//    
-// })
+
+//need to get the chosen sign from local storage to profile div. 
+const signChoice = document.getElementById('#aries');
+console.log(signChoice)
+
+const signProfile = document.getElementById('star-sign');
+console.log(signProfile)
+
+// signChoice.addEventListener('click', displayUserChoice)
+getUserChoice()
+// I think I have to do if or a switch case to tie the buttons to a function I can hook into. 
+function getUserChoice(){
+    localStorage.setItem('Sign', onclick.value)
+    let sign = localStorage.getItem('Sign')
+    console.log(Sign)
+    displayUserChoice()
+    function displayUserChoice () {
+        signChoice.onclick = function () {
+            signChoice.textContent = sign;
+            console.log('sign')
+        }
+    }
+}
 const URL = 'https://aztro.sameerkumar.website/?sign=aries&day=today';
 fetch(URL, {
     method: 'POST'
